716068a43c9d7ed418e460b3d49b17163a1a25ab
Artem <Strelocek@mail.ru>
2024-07-05 00:27:14 +0300

    123

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func (s ParcelService) PrintClientParcels(client int) error {
 
 	fmt.Printf("Посылки клиента %d:\n", client)
 	for _, parcel := range parcels {
-		fmt.Printf("Посылка № %d на адрес %s от клиента с идентификатором %d зарегистрирована %s, статус %s\n",
+		fmt.Printf("Посилка № %d на адрес %s от клиента с идентификатором %d зарегистрирована %s, статус %s\n",
 			parcel.Number, parcel.Address, parcel.Client, parcel.CreatedAt, parcel.Status)
 	}
 	fmt.Println()
